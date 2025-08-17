bf69e06fb1bfad7e8dcc24d64f97330e96d4063d
mezzaluna0723-arch <mezzaluna0723@gmail.com>
2025-08-18 00:01:08 +0700

    scroll html, scroll css, scroll js

---
+++ scroll.js
@@ -0,0 +1,22 @@
+// Select all elements with class fade-in
+const faders = document.querySelectorAll('.fade-in');
+
+// Intersection Observer API
+const appearOptions = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('visible');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
